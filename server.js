a520f7b688bf7f8370a76e03b70815158a1d8331
Sameer B <sameerflyleaf@gmail.com>
2022-10-01 16:41:23 -0700

    bug in apiRoute.js and uuidv1 is getting ERR_PACKAGE_PATH_NOT_EXPORTED error. cant load server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const htmlRoute = require("./routes/htmlRoute");
-
+const apiRoute = require("./routes/apiRoute");
 const app = express();
 
 app.use(express.json());
@@ -10,7 +10,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //routes
-app.use("/", htmlRoute)
+app.use("/", htmlRoute);
+app.use("/api", apiRoute);
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT#: ${PORT}`)
